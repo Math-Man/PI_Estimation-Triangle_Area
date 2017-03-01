6d24969acf38107d5ee42f8de02878c4044138e7
Math-Man <kayacan.goktug@gmail.com>
2017-03-01 19:55:46 +0300

    updated comments

--- src/main.java
+++ src/main.java
@@ -2,11 +2,7 @@
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//WHY DID IT NOT WORK AT CLASS?
-		//In short, I forgat to put a pair of operations between parenthesis
-		//Which caused a program at calculation like multiplying radius by the amount of points in the circle.
-
+		
 		javafx.application.Application.launch(menuManager.class);
 
 	}
